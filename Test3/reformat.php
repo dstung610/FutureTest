a5ac6f52a46dce66066c210b1b679adedd54297e
Son Tung Do <dstung610@gmail.com>
2021-02-03 13:23:14 +0100

    +update test 3 +replace damaged data file

--- Test3/reformat.php
+++ Test3/reformat.php
@@ -1,28 +1,76 @@
 <?php
 $output = "output/";
-$dir = new RecursiveDirectoryIterator('data/');
-foreach (new RecursiveIteratorIterator($dir) as $filename => $file) {
-    // $file = "data/20131004/device-tokens-for-sfx-collection-1.log";
+// $dir = new RecursiveDirectoryIterator('data/');
+// foreach (new RecursiveIteratorIterator($dir) as $filename => $file) {
+$file = "data/20131004/device-tokens-for-sfx-collection-1.log";
+if (!is_dir($file)) {
     $fp = fopen($file, "r");
     if ($fp !== FALSE) {
-        // $fpw = fopen("/output/new-" . $file, "w");
-        $fpw = fopen("/new-" . $file, (file_exists($file)) ? 'a' : 'w');
+        $fpw = fopen("output/" . pathinfo($file)['filename'] . "-mod.csv", (file_exists($file)) ? 'a' : 'w');
         if ($fpw !== FALSE) {
-            $list = array();
-            $row = array();
+            $header = array(
+                "id",
+                "appCode",
+                "deviceId",
+                "contactable",
+                "subscription_status",
+                "has_downloaded_free_product_status",
+                "has_downloaded_iap_product_status"
+            );
+            fputcsv($fpw, $header);
+            $flag = 0;
+            $i = 0;
             while (($data = fgetcsv($fp, 1000, ",")) !== FALSE) {
-                $num = count($data);
-                for ($i = 0; $i < $num; $i++) {
-                    // echo $data[$i] . "\n";
-                    array_push($row, $data[$i]);
+                if ($flag == 1) {
+                    $row = array();
+                    array_push($row, $i); //id
+                    array_push($row, $data[0]); //Appcode
+                    array_push($row, $data[1]); //deviceId = deviceToken
+                    array_push($row, $data[2]); //Contactable = deviceTokenStatus
+
+                    $taglist = explode("|", $data[3]);
+                    $substat = "";
+                    $downfreestat = "";
+                    $downiapstat = "";
+                    $currentTag = "";
+                    
+                    for ($j = 0; $j <= count($taglist); $j++) {
+                        $currentTag = $taglist[$j];
+                        if (strcmp($currentTag, "active_subscriber") || strcmp($currentTag, "expired_subscriber") || strcmp($currentTag, "never_subscribed") || strcmp($currentTag, "subscription_unknown")) {
+                            $substat = $currentTag;
+                            break;
+                        }
+                    }
+                    $currentTag = "";
+                    for ($j = 0; $j <= count($taglist); $j++) {
+                        $currentTag = $taglist[$j];
+                        if (strcmp($currentTag, "has_downloaded_free_product") || strcmp($currentTag, "not_downloaded_free_product") || strcmp($currentTag, "downloaded_free_product_unknown")) {
+                            $downfreestat = $currentTag;
+                            break;
+                        }
+                    }
+                    $currentTag = "";
+                    for ($j = 0; $j <= count($taglist); $j++) {
+                        $currentTag = $currentTag;
+                        if (strcmp($currentTag, "has_downloaded_iap_product") || strcmp($currentTag, "not_downloaded_free_product") || strcmp($currentTag, "downloaded_iap_product_unknown")) {
+                            $downiapstat = $currentTag;
+                            break;
+                        }
+                    }
+
+                    array_push($row, $substat); //subscriptionStatus
+                    array_push($row, $downfreestat); //has_downloaded_free_product_status",                
+                    array_push($row, $downiapstat); //"has_downloaded_iap_product_status"
+
+                    fputcsv($fpw, $row);
+                    $i++;
+                } else {
+                    $flag++;
                 }
-                array_push($list, $row);
-            }
-            foreach ($list as $value) {
-                fputcsv($fpw, $value);
             }
             fclose($fpw);
         }
     }
     fclose($fp);
 }
+// }
